5898439646f5d902c37af70686fe38f28c43066c
Aaron Madison <aaron.l.madison@gmail.com>
2011-07-23 04:55:34 -0500

    moved to a folder and made into an installable package

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+
+
+from blogger import VERSION
+
+setup(
+    name="django-blogger",
+    description="A clone of a Blogger blog",
+    version = VERSION,
+    author="aaron madison",
+    author_email="aaron.l.madison@gmail.com",
+
+    long_description=open("README.txt", 'r').read(),
+    packages=find_packages(),
+    test_requirements=['django'],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
