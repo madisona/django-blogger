bf2645dd4a44eb29adf36e84e960e99fc17564c3
Aaron Madison <aaron.l.madison@gmail.com>
2011-07-31 14:51:14 -0500

    added csrf_exempt to pubsubhubbub url

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls.defaults import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 
 from blogger import views
 
@@ -7,7 +8,7 @@ from blogger import views
 # (r'^', include('blogger.urls', namespace='blogger')),
 
 urlpatterns = patterns('blogger.views',
-    url(r'^blog/hubbub/', views.PubSubHubbub.as_view(), name="hubbub"),
+    url(r'^blog/hubbub/', csrf_exempt(views.PubSubHubbub.as_view()), name="hubbub"),
     url(r'^blog/(?P<year>\d{4})/$', views.ArchiveYear.as_view(), name='archive_year'),
     url(r'^blog/(?P<year>\d{4})/(?P<month>\w+)/$', views.ArchiveMonth.as_view(), name='archive_month'),
     url(r'^blog/(?P<slug>[\w/-]+)/$', views.PostDetail.as_view(), name='post'),
