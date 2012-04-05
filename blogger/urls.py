cc1eb2a06467010f447572fb9f6975e558573b90
Aaron Madison <aaron.l.madison@gmail.com>
2012-04-05 05:35:56 -0500

    removed "blog" from the front of all urls... should be up to project to determine that

--- blogger/urls.py
+++ blogger/urls.py
@@ -8,10 +8,10 @@ from blogger import views
 # (r'^', include('blogger.urls', namespace='blogger')),
 
 urlpatterns = patterns('blogger.views',
-    url(r'^blog/pubsubhubbub/', csrf_exempt(views.PubSubHubbub.as_view()), name="hubbub"),
-    url(r'^blog/(?P<year>\d{4})/$', views.ArchiveYear.as_view(), name='archive_year'),
-    url(r'^blog/(?P<year>\d{4})/(?P<month>\w+)/$', views.ArchiveMonth.as_view(), name='archive_month'),
-    url(r'^blog/(?P<slug>[\w/-]+)/$', views.PostDetail.as_view(), name='post'),
+    url(r'^pubsubhubbub/', csrf_exempt(views.PubSubHubbub.as_view()), name="hubbub"),
+    url(r'^(?P<year>\d{4})/$', views.ArchiveYear.as_view(), name='archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w+)/$', views.ArchiveMonth.as_view(), name='archive_month'),
+    url(r'^(?P<slug>[\w/-]+)/$', views.PostDetail.as_view(), name='post'),
 
     url(r'^$', views.PostList.as_view(), name='home'),
 )
