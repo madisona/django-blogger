dd2a181ca3f24249bee537dd8c59afe2537cb95b
Aaron Madison <aaron.l.madison@gmail.com>
2012-04-05 05:49:22 -0500

    added template tags to access Archive year list and teaser snippets

--- blogger/templatetags/blogger_tags.py
+++ blogger/templatetags/blogger_tags.py
@@ -1,10 +1,25 @@
 
 from django import template
 
-from blogger import config, models
+from blogger import models
 
 register = template.Library()
 
 @register.assignment_tag
 def get_recent_posts(cnt=None):
     return models.BloggerPost.get_latest_posts(cnt)
+
+
+@register.inclusion_tag('blogger/teaser_snippet.html')
+def render_latest_blog_posts(num):
+    posts = models.BloggerPost.get_latest_posts(num)
+    return {
+        'blog_posts': posts,
+    }
+
+
+@register.inclusion_tag('blogger/archive_month_links_snippet.html')
+def render_month_links():
+    return {
+        'dates': models.BloggerPost.objects.all().dates('published', 'month'),
+    }
